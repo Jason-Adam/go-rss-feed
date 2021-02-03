9bdb90a8a13484f85b8c043581aeeb3c37f0ea03
Jason Adam <22adamj22@gmail.com>
2021-02-02 21:56:10 -0700

    fixed go target env to 32 bit arm for pi

--- run_emailer.sh
+++ run_emailer.sh
@@ -1,7 +1,14 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-cd ~/code/go-rss-feed && \
+set -e;
+
+curl -s https://api.github.com/repos/jason-adam/go-rss-feed/releases/latest \
+    | grep "browser_download_url" \
+    | cut -d '"' -f 4 \
+    | wget -qi -;
+
+tar -xvf go-rss-feed-linux-arm32.tar.gz && \
     set -o allexport && \
-    source dev.env && \
+    source ~/envs/go-rss-feed.env && \
     set +o allexport && \
-    ./go-rss-feed;
+    ./go-rss-feed-linux-arm32;
