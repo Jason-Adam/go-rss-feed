824aad606a8e316043d7af9e593a62b55c16fda0
Jason Adam <22adamj22@gmail.com>
2021-06-03 20:22:12 -0700

    refactored run emailer script

--- run_emailer.sh
+++ run_emailer.sh
@@ -2,12 +2,18 @@
 
 set -e
 
+# Get latest release
 curl -s https://api.github.com/repos/jason-adam/go-rss-feed/releases/latest |
     grep "browser_download_url" |
     cut -d '"' -f 4 |
     wget -qi -
 
+# Unzip and move to repo
 tar -xvf go-rss-feed-linux-arm32.tar.gz &&
     mv go-rss-feed-linux-arm32 "$HOME"/code/go-rss-feed &&
     cd "$HOME"/code/go-rss-feed &&
     ./go-rss-feed-linux-arm32
+
+# Cleanup
+rm "$HOME"/go-rss-feed-linux-arm32.tar.gz
+rm "$HOME"/code/go-rss-feed/go-rss-feed-linux-arm32
